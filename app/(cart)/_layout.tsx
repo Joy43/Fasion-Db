9da762322b088b4c8d2f669fba948c09f6ad2e11
Joy43 <ssjoy43@gmail.com>
2025-06-19 23:59:27 +0600

    cart show data

---
+++ app/(cart)/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function CartLayout() {
+  return (
+    <View>
+      <Text>CartLayout</Text>
+    </View>
+  )
+}
